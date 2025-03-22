39dfdab1e491cbdfd46324c6afa0b0597bc99cc6
nisharga <kabirnisharga@gmail.com>
2025-03-22 22:56:46 +0600

    pwa app create

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,8 @@
 import {
+    APP_DEFAULT_TITLE,
+    APP_DESCRIPTION,
+    APP_NAME,
+    APP_TITLE_TEMPLATE,
     FONT_DEFAULT,
     SITE_DESCRIPTION_DEFAULT,
     SITE_DOMAIN,
@@ -7,13 +11,15 @@ import {
     SITE_VERIFICATION_GOOGLE_DEFAULT,
     switchThemeDuration
 } from '@/config';
-import { Metadata } from 'next';
+import { Metadata, Viewport } from 'next';
 import React from 'react';
 import '../styles/global.css';
-import { ReduxProviders } from '@/providers';
+import { ReduxProviders } from '@/providers'; 
+
 
 export const metadata: Metadata = {
     metadataBase: new URL(SITE_DOMAIN),
+    applicationName: APP_NAME,
     title: {
         default: SITE_TITLE_DEFAULT,
         template: SITE_TITLE_TEMPLATE_DEFAULT
@@ -22,8 +28,40 @@ export const metadata: Metadata = {
     verification: {
         google: SITE_VERIFICATION_GOOGLE_DEFAULT
     },
-    icons: '/favicon.ico'
+    icons: '/favicon.ico',
+    manifest: '/manifest.json',
+    appleWebApp: {
+        capable: true,
+        statusBarStyle: 'default',
+        title: APP_DEFAULT_TITLE 
+    },
+    formatDetection: {
+        telephone: false
+    },
+    openGraph: {
+        type: 'website',
+        siteName: APP_NAME,
+        title: {
+            default: APP_DEFAULT_TITLE,
+            template: APP_TITLE_TEMPLATE
+        },
+        description: APP_DESCRIPTION
+    },
+    twitter: {
+        card: 'summary',
+        title: {
+            default: APP_DEFAULT_TITLE,
+            template: APP_TITLE_TEMPLATE
+        },
+        description: APP_DESCRIPTION
+    }
 };
+
+
+export const viewport: Viewport = {
+    themeColor: '#FFFFFF'
+};
+
 
 export default function RootLayout({
     children
