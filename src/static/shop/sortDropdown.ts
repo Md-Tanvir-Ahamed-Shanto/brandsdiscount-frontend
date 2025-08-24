3d0987b176e42c4e52cc46ff31ddffecb0c65507
Md Tanvir Ahamed Shanto <mdtanvirahamdsanto987@gmail.com>
2025-08-24 23:57:13 +0600

    sdf

--- src/static/shop/sortDropdown.ts
+++ src/static/shop/sortDropdown.ts
@@ -1,10 +1,4 @@
 export const sortOptions = [ 
-    { value: 'regularPrice_asc', label: 'Price: Low to High' },
-    { value: 'regularPrice_desc', label: 'Price: High to Low' },
-    { value: 'discountPercent_asc', label: "Discount: Low to High" },
-    { value: 'discountPercent_desc', label: 'Discount: High to Low' },
-    { value: 'salePrice_asc', label: 'Sale Price: Low to High' },
-    { value: 'salePrice_desc', label: 'Sale Price: High to Low' },
-    { value: 'stockQuantity_asc', label: 'Stock: Low to High' },
-    { value: 'stockQuantity_desc', label: 'Stock: High to Low' }
+    { value: 'lowToHigh', label: 'Price: Low to High' },
+    { value: 'highToLow', label: 'Price: High to Low' },
 ];
