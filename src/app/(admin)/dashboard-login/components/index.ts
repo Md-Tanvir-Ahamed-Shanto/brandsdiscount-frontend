d45b4b81ef58c8c24dee754e10803641d0004cad
Md Tanvir Ahamed Shanto <mdtanvirahamdsanto987@gmail.com>
2025-10-19 22:11:37 +0600

    refactor(admin): remove admin dashboard and related components

--- src/app/(admin)/dashboard-login/components/index.ts
+++
@@ -1,3 +0,0 @@
-export { default as LoginSection } from './LoginSection';
-export { default as LoginSidebar } from './LoginSidebar';
-export { default as LoginForm } from './LoginForm'; 
--- src/app/(admin)/dashboard/components/index.ts
+++
@@ -1,3 +0,0 @@
-export { default as AdminPanel } from './AdminPanel';
-export { default as ChangePasswordAccordion } from './ChangePasswordAccordion';
-export { default as UserProfile } from './UserProfile';
